8b209ec1ec8d9819edd086457f7413a9a230e11d
James Njuguna <jamesnjugunan998@gmail.com>
2024-06-30 07:29:44 +0300

    get user by id

--- controllers/authController.js
+++ controllers/authController.js
@@ -184,3 +184,33 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
     message: 'Logged out'
   })
 })
+
+
+//admin routes
+
+//get all users => /api/v1/admin/users
+
+exports.allUsers = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users
+  })
+})
+
+
+//get user details =>/api/v1/admin/user:id
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if(!user) {
+    return next(new ErrorHandler(`User not found with id: ${req.params.id}`))
+  }
+
+  res.status(200).json({
+    success: true,
+    user
+  })
+})
+
--- routes/auth.js
+++ routes/auth.js
@@ -10,9 +10,11 @@ const {
   getUserProfile, 
   updatePassword,
   updateProfile,
+  allUsers,
+  getUserDetails,
 } = require('../controllers/authController');
 
-const { isAuthenticatedUser  } = require('../middlewares/auth')
+const { isAuthenticatedUser, authorizeRoles  } = require('../middlewares/auth')
 
 
 
@@ -21,9 +23,12 @@ router.route('/login').post(loginUser);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
 router.route('/logout').get(logout);
-router.route('/me').get(isAuthenticatedUser, getUserProfile)
-router.route("/password/update").put(isAuthenticatedUser, updatePassword)
-router.route("/me/update").put(isAuthenticatedUser, updateProfile)
+router.route('/me').get(isAuthenticatedUser, getUserProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
+router.route("/me/update").put(isAuthenticatedUser, updateProfile);
+router.route("/admin/users").get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
+router.route("/admin/user/:id").get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails);
+
 
 
 module.exports = router;
