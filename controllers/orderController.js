ec410416c1c4f4a04d47e15d758d37b1ca72444b
James Njuguna <jamesnjugunan998@gmail.com>
2024-07-03 00:18:03 +0300

    update/process order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -62,7 +62,7 @@ exports.myOrders = catchAsyncErrors(async(req, res, next) =>{
 })
 
 
-//get all orders => /api/v1/admin/orders
+//get all orders - ADMIN => /api/v1/admin/orders
 
 exports.allOrders = catchAsyncErrors(async(req, res, next) =>{
   const orders = await Order.find();
@@ -80,3 +80,34 @@ exports.allOrders = catchAsyncErrors(async(req, res, next) =>{
     orders
   })
 })
+
+//update/process orders - ADMIN => /api/v1/admin/order/:id
+
+exports.updateOrders = catchAsyncErrors(async(req, res, next) =>{
+  const order = await Order.findById(req.params.id);
+
+ if(order.orderStatus === 'Delivered') {
+  return next(new ErrorHandler('You have already delivered this order', 400))
+ }
+
+ order.orderItems.forEach(async item => {
+  await updateStock(item.product, item.quantity)
+ })
+
+order.orderStatus =  req.body.status, 
+order.deliveredAt = Date.now()
+
+await order.save();
+
+  res.status(200).json({
+    success: true
+  })
+})
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  product.stock = product.stock - quantity;
+
+  await product.save({validateBeforeSave: false});
+}
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = app.listen(PORT, () => {
 //handle unhandled promise rejection errors
 
 process.on('unhandledRejection', err => {
-    console.log(`ERROR: ${err.message}`);
+    console.log(`ERROR: ${err.stack}`);
     console.log('Shutting down server due to unhandled Promise Rejection')
     server.close(() => {
         process.exit(1)
